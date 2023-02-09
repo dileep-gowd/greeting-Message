ad34f2c9a312c3e4d41f69caef6901bc02c3099b
dileep-gowd <srisailamdileep22@gmail.com>
2023-02-09 12:54:31 +0000

    Node JS solution

---
+++ message/index.js
@@ -0,0 +1,8 @@
+const greet = require("../greeting/index");
+//const greeting = "Hello Rahul! Have a Great Day";
+
+//exports.greeting = greeting;
+
+const res = `Hello Rahul! ${greet}`;
+module.exports = res;
+//console.log(res);
